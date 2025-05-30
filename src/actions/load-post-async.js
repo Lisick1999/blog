91bcc267190f1f15d7d6787ee43a1816e74760ee
Fox Say <Lisick1999@yandex.ru>
2025-05-30 23:56:36 +0300

    Реализация доступа к кнопкам

--- src/actions/load-post-async.js
+++ src/actions/load-post-async.js
@@ -1,4 +1,3 @@
-// асинхронный экшен , который позволяет диспатчить другие экшены
 import { setPostData } from './set-post-data';
 
 export const loadPostAsync = (requestServer, postId) => (dispatch) =>
