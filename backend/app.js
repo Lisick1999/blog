704228c34b44c5226cc6ef6d9ca5f3507e4b805d
Lisick1999 <Lisick1999@yandex.ru>
2025-06-19 22:47:25 +0300

    подключение докера

--- backend/app.js
+++ backend/app.js
@@ -1,25 +1,62 @@
+// require('dotenv').config();
+
+// const express = require('express');
+// const mongoose = require('mongoose');
+// const cookieParser = require('cookie-parser');
+// const routes = require('./routes');
+
+// const port = 3001; // порт, на котором будет запускаться приложение
+// const app = express(); // экземпляр приложения
+
+// // подключение мидлвэеров
+// app.use(express.static('../frontend/build'))
+
+// app.use(cookieParser());
+// app.use(express.json());
+
+// app.use('/api', routes);
+
+// // подключение к базе данных
+// mongoose.connect(process.env.DB_CONNECTION_STRING).then(() => {
+// 	// после подключения к базе запустить приложение
+// 	app.listen(port, () => {
+// 		console.log(`Сервер запущен на порту ${port}`);
+// 	});
+// });
+
 require('dotenv').config();
 
 const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
+const cors = require('cors'); // Импортируем cors
 const routes = require('./routes');
 
 const port = 3001; // порт, на котором будет запускаться приложение
 const app = express(); // экземпляр приложения
 
-// подключение мидлвэеров
-app.use(express.static('../frontend/build'))
+// Настройка CORS для разрешения запросов с любого источника
+app.use(cors({
+  origin: '*', // Разрешаем запросы с любого источника
+  credentials: true, // Разрешаем отправку куки и заголовков авторизации
+}));
+
+// Подключение мидлвэеров
+app.use(express.static('../frontend/build'));
 
 app.use(cookieParser());
 app.use(express.json());
 
 app.use('/api', routes);
 
-// подключение к базе данных
+app.get("*", (req, res) => {
+	res.sendFile(patch.resolve('..', 'frontend', 'build', 'index.html'))
+})
+
+// Подключение к базе данных
 mongoose.connect(process.env.DB_CONNECTION_STRING).then(() => {
-	// после подключения к базе запустить приложение
-	app.listen(port, () => {
-		console.log(`Сервер запущен на порту ${port}`);
-	});
+  // После подключения к базе запустить приложение
+  app.listen(port, () => {
+    console.log(`Сервер запущен на порту ${port}`);
+  });
 });
